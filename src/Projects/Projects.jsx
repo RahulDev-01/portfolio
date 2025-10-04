a7d94c8a8cbb0ac1bb6043eb00c377e6f02eba4c
S.Rahul <s.rahul5116@outlook.com>
2025-10-04 23:15:21 +0530

    Map Project Added :rabbit:

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -98,7 +98,7 @@ function Projects() {
               </CardBody>
             </CardContainer>
             {/* Right Section */}
-            <div className='bg-black/20 backdrop-blur-sm w-full flex h-[400px] md:h-[450px] p-4 md:p-6 lg:p-10 rounded-xl border border-purple-400/30 mt-4 lg:mt-8'>
+            <div className='bg-black/20 backdrop-blur-sm w-full flex h-[400px] md:h-[450px] p-4 md:p-6 lg:p-10 rounded-xl border border-purple-400/30 mt-4 lg:mt-8 hover:shadow-2xl hover:shadow-purple-500/[0.3] transition-all duration-300 hover:scale-102 hover:-translate-y-1'>
             <div className="flex justify-center items-center h-full flex-col px-2 md:px-4">
       <p className="text-neutral-500 dark:text-neutral-400 text-sm md:text-base lg:text-lg xl:text-2xl max-w-xs sm:max-w-sm md:max-w-lg lg:max-w-xl xl:max-w-3xl mx-auto mb-4 md:mb-6 lg:mb-10">
         <LinkPreview
@@ -156,7 +156,7 @@ function Projects() {
               </div>
             </CardBody>
         </CardContainer>
-        <div className='bg-black/20 backdrop-blur-sm w-full flex h-[400px] md:h-[450px] p-4 md:p-6 lg:p-10 rounded-xl border border-purple-400/30 mt-4 lg:mt-8'>
+        <div className='bg-black/20 backdrop-blur-sm w-full flex h-[400px] md:h-[450px] p-4 md:p-6 lg:p-10 rounded-xl border border-purple-400/30 mt-4 lg:mt-8 hover:shadow-2xl hover:shadow-purple-500/[0.3] transition-all duration-300 hover:scale-102 hover:-translate-y-1'>
           <div className="flex justify-center items-center h-full flex-col px-2 md:px-4">
             <p className="text-neutral-500 dark:text-neutral-400 text-sm md:text-base lg:text-lg xl:text-2xl max-w-xs sm:max-w-sm md:max-w-lg lg:max-w-xl xl:max-w-3xl mx-auto mb-4 md:mb-6 lg:mb-10">
               <LinkPreview
@@ -173,6 +173,66 @@ Result Display: The app shows the results once the prompt is sent, including a l
             <p className="text-neutral-500 dark:text-neutral-400 text-sm md:text-base lg:text-lg xl:text-2xl max-w-xs sm:max-w-sm md:max-w-lg lg:max-w-xl xl:max-w-3xl mx-auto mb-4 md:mb-6 lg:mb-10">
               Technologies Used 🧰: React.js, React Router, Sonner, Tailwind, Google OAuth, Vite ⚡, Version Management {" "}
         <LinkPreview url="https://github.com/RahulDev-01/Gemini-clone" className="font-bold text-blue-400">
+          Git-Hub 
+        </LinkPreview>{" "}
+        and My GitHub Profile {" "} 
+        <LinkPreview url="https://github.com/RahulDev-01" className="font-bold text-red-700">
+          RahulDev-01 
+        </LinkPreview>{" "}, Website is also hosted in Vercel Platform.       
+            </p>
+          </div>
+
+        </div>
+      </div>
+      <div className='w-full flex flex-col lg:flex-row h-auto lg:h-[600px] p-4 md:p-6 lg:p-10 rounded-xl gap-4 lg:gap-5'>
+        <CardContainer className="inter-var">
+          <CardBody className="bg-black/20 backdrop-blur-md relative group/card hover:shadow-2xl hover:shadow-purple-500/[0.3] border-purple-400/30 w-full lg:w-auto lg:sm:w-[30rem] h-[400px] md:h-[450px] rounded-xl p-4 md:p-6 border">
+            <CardItem translateZ="50" className="text-lg md:text-xl font-bold text-neutral-600 dark:text-white ">
+            Interactive Map 🗺️   
+            </CardItem>
+            <CardItem as="p" translateZ="60" className="text-neutral-500 text-xs md:text-sm max-w-sm mt-2 dark:text-neutral-300">
+            This project is a responsive web application displaying an interactive map with multiple city markers. It allows users to toggle between the standard Open Street Map and Satellite .
+            </CardItem>
+            <CardItem translateZ="100" className="w-full mt-4">
+              <img
+                src="/Projects/Map.png"
+                height="1000"
+                width="1000"
+                className="h-48 md:h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl transition-transform duration-300 group-hover/card:scale-105 group-hover/card:-translate-y-1"
+                alt="thumbnail"
+              />
+              </CardItem>
+              <div className="flex justify-between items-center m-4">
+                <CardItem
+                  translateZ={20}
+                  as="a"
+                  href="https://map-rahul.netlify.app/"
+                  target="_blank"
+                  className="px-3 md:px-4 py-2 rounded-xl text-xs font-normal text-gray-100  hover:from-purple-600 hover:to-pink-600 transition-all duration-300 transform hover:scale-105 hover:-translate-y-1 hover:shadow-lg hover:shadow-purple-500/50"
+                >
+                  Click Here →
+                </CardItem>
+              </div>
+            </CardBody>
+        </CardContainer>
+        <div className='bg-black/20 backdrop-blur-sm w-full flex h-[400px] md:h-[450px] p-4 md:p-6 lg:p-10 rounded-xl border border-purple-400/30 mt-4 lg:mt-8 hover:shadow-2xl hover:shadow-purple-500/[0.3] transition-all duration-300 hover:scale-102 hover:-translate-y-1'>
+          <div className="flex justify-center items-center h-full flex-col px-2 md:px-4">
+            <p className="text-neutral-500 dark:text-neutral-400 text-sm md:text-base lg:text-lg xl:text-2xl max-w-xs sm:max-w-sm md:max-w-lg lg:max-w-xl xl:max-w-3xl mx-auto mb-4 md:mb-6 lg:mb-10">
+              <LinkPreview
+                url="https://map-rahul.netlify.app/"
+                className="font-bold bg-clip-text text-transparent bg-gradient-to-br from-purple-500 to-pink-500"
+              >
+                Interactive Map 
+              </LinkPreview>{" "}
+              🗺️  Interactive Map rendered with Leaflet.js 🌍
+Markers for Delhi, Mumbai, Kolkata, Bengaluru, and New York 🏙️,  with info popups 💬
+Zoom Controls on the top left 🔍
+Satellite View Toggle Button positioned directly below the zoom controls 🌐
+Button icon is fetched dynamically from an API 🔄 .
+            </p>
+            <p className="text-neutral-500 dark:text-neutral-400 text-sm md:text-base lg:text-lg xl:text-2xl max-w-xs sm:max-w-sm md:max-w-lg lg:max-w-xl xl:max-w-3xl mx-auto mb-4 md:mb-6 lg:mb-10">
+              Technologies Used 🧰: React.js, React Router, Sonner, Tailwind, Google OAuth, Vite ⚡, Version Management {" "}
+        <LinkPreview url="https://github.com/RahulDev-01/interactive-map-design" className="font-bold text-blue-400">
           Git-Hub 
         </LinkPreview>{" "}
         and My GitHub Profile {" "} 
